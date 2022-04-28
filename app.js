c2bb57b016efcf1135160953d58a1b8aebd935b7
HarlemMauriceBrooklyn <admin_nyc@macbook-pro.myfiosgateway.com>
2022-04-28 10:11:24 -0400

    k

--- app.js
+++ app.js
@@ -14,36 +14,98 @@
 // {A}: 6 ships
 
 // ship ={
-    // hull:
-    // firepower:
-    // accuracy:
+// hull:
+// firepower:
+// accuracy:
 // }
-
 
 // Guile Throw https://onlinegiftools.com/images/examples-onlinegiftools/sonic-boom.gif
 
-    // https://www.fightersgeneration.com/characters/charlie-sonicshell.gif
+// https://www.fightersgeneration.com/characters/charlie-sonicshell.gif
+
+// ----{  coudla done this  }
+// function Alien(lifePoints,firePower,accuracy){
+//     this.lifePoints = lifePoints
+//     this.firePower = firePower
+//     this.accuracy = accuracy
+// }
+
+let USS_HolaMundo = {
+  lifePoints: 100,
+  firepower: 10,
+  accuracy: 10,
+  attackAlien: function(){
+    let enemyPosition = prompt(" Ready To Attack? ");
+
+    if (enemyPosition === "y") {
+      let relevantTarget = document.querySelector(".counter");
+      let bomb = document.createElement("img");
+      bomb.setAttribute(
+        "src",
+        "https://i.gifer.com/origin/d7/d7ac4f38b77abe73165d85edf2cbdb9e_w200.gif"
+      );
+      bomb.style.height = "60px";
+      relevantTarget.replaceWith(bomb);
+      
+      console.log("ATTACKING!>!>!!!");
+  
+      let ship = document.querySelector(".ussr");
+      ship.classList.toggle("pOneAttack");
+      // let x = document.getElementById()
+    }
+  }
+};
+
+let alienOne = {
+  lifePoints: 100,
+  firepower: 10,
+  accuracy: 10,
+};
+
+let alienTwo = {
+  lifePoints: 50,
+  firepower: 10,
+  accuracy: 5,
+};
+let alienGang = [alienOne, alienTwo];
+
+const showDirections = () => {
+  alert(
+    "Earth has been attacked by a horde of aliens! You are the captain of the USS HelloWorld, on a mission to destroy every last alien ship.Battle the aliens as you try to destroy them with your lasers.There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat."
+  );
+};
 
 
-const pOneAttack = () => {
-    let ship = document.querySelector('.ussr')
-        // create New Action
-            let newImg = document.createElement('img')
-                newImg.setAttribute('src','https://onlinegiftools.com/images/examples-onlinegiftools/sonic-boom.gif')
-                newImg.setAttribute('class', 'ussr');
-        ship.replaceWith(newImg)
-        
-        ship.classList.toggle('pOneAttack')
+const attackAlien = () => {
+  // targetAlien & decrease Points
+  let enemyPosition = prompt(" Ready To Attack? ");
+
+  if (enemyPosition === "y") {
+    let relevantTarget = document.querySelector(".counter");
+    let bomb = document.createElement("img");
+    bomb.setAttribute(
+      "src",
+      "https://i.gifer.com/origin/d7/d7ac4f38b77abe73165d85edf2cbdb9e_w200.gif"
+    );
+    bomb.style.height = "60px";
+    relevantTarget.replaceWith(bomb);
+    
+    console.log("ATTACKING!>!>!!!");
+
+    let ship = document.querySelector(".ussr");
+    ship.classList.toggle("pOneAttack");
+    // let x = document.getElementById()
+  }
+};
+
+const objTest = (nput) =>{
+    console.log(nput.email)
 }
 
-const absorbDamage = (alienId) => {
-
-    let victim = document.querySelector(alienId)
-    victim.innerHTML = score - 100
+let me = {
+    name: "Jordan",
+    email: "a@a.com"
 }
 
-const fireShot = () => {
-    let laser = prompt("Which Position Would You Like To Fire At ?")
 
-}
 
