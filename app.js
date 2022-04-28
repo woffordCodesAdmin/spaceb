f82f8528a9a5e4a13af82ef3a4e7d6bbf12ab7d7
HarlemMauriceBrooklyn <admin_nyc@macbook-pro.myfiosgateway.com>
2022-04-27 20:09:51 -0400

    initialCommit:

---
+++ app.js
@@ -0,0 +1,22 @@
+// Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship.
+
+// Battle the aliens as you try to destroy them with your lasers.
+
+// There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.
+
+// Aliens vs Humans - GAME
+
+// {H}: GO First
+//  Only can Attack in Order
+
+// After DestroyedShip = option2retreat
+
+// {A}: 6 ships
+
+// ship ={
+    // hull:
+    // firepower:
+    // accuracy:
+// }
+
+
